e32a561f417a30f9b1dae0b7e8b82b5c3c05eec9
林小铂 <491325432@qq.com>
2021-04-18 11:26:49 +0800

    Validate configuration in SinkConfigurationFactory

--- src/main/java/mongoflink/config/SinkConfigurationFactory.java
+++ src/main/java/mongoflink/config/SinkConfigurationFactory.java
@@ -1,5 +1,6 @@
 package mongoflink.config;
 
+import org.apache.flink.util.Preconditions;
 import org.apache.flink.util.PropertiesUtil;
 
 import java.util.Properties;
@@ -16,9 +17,20 @@ public class SinkConfigurationFactory {
         configuration.setFlushOnCheckpoint(
                 PropertiesUtil.getBoolean(properties, MongoOptions.SINK_FLUSH_ON_CHECKPOINT, false));
         configuration.setBulkFlushSize(
-                PropertiesUtil.getLong(properties, MongoOptions.SINK_FLUSH_SIZE, 1000L));
+                PropertiesUtil.getLong(properties, MongoOptions.SINK_FLUSH_SIZE, 1_000L));
         configuration.setBulkFlushInterval(
                 PropertiesUtil.getLong(properties, MongoOptions.SINK_FLUSH_INTERVAL, 30_000L));
+
+        // validate config
+        if (configuration.isTransactional()) {
+            Preconditions.checkArgument(configuration.isFlushOnCheckpoint(),
+                    "`%s` must be true when the transactional sink is enabled",
+                    MongoOptions.SINK_FLUSH_ON_CHECKPOINT);
+        }
+        Preconditions.checkArgument(configuration.getBulkFlushSize() > 0,
+                "`%s` must be greater than 0",
+                MongoOptions.SINK_FLUSH_SIZE);
+
         return configuration;
     }
 }
