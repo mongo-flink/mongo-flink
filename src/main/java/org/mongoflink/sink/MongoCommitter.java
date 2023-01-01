54b3466b34597b9132d68e79e6c1f013a35b2c83
Saketh Kurnool <kurnoolsaketh@gmail.com>
2022-12-31 19:53:59 -0800

    remove chaos monkey conditions, add comment explaining: - how redundant insertions are handled - how redundant upserts are handled
    at this point, the sink should be exactly once.

--- src/main/java/org/mongoflink/sink/MongoCommitter.java
+++ src/main/java/org/mongoflink/sink/MongoCommitter.java
@@ -1,10 +1,10 @@
 package org.mongoflink.sink;
 
-import com.mongodb.*;
 import org.mongoflink.internal.connection.MongoClientProvider;
 
 import org.apache.flink.api.connector.sink.Committer;
 
+import com.mongodb.*;
 import com.mongodb.client.ClientSession;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
@@ -16,7 +16,6 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 /**
  * MongoCommitter flushes data to MongoDB in a transaction. Due to MVCC implementation of MongoDB, a
@@ -63,7 +62,6 @@ public class MongoCommitter implements Committer<DocumentBulk> {
     public List<DocumentBulk> commit(List<DocumentBulk> committables) throws IOException {
         List<DocumentBulk> failedBulk = new ArrayList<>();
 
-        int rand = new Random().nextInt(10 - 1 + 1) + 1;
         for (DocumentBulk bulk : committables) {
             if (bulk.getDocuments().size() > 0) {
                 CommittableTransaction transaction;
@@ -75,39 +73,37 @@ public class MongoCommitter implements Committer<DocumentBulk> {
                     transaction = new CommittableTransaction(collection, bulk.getDocuments());
                 }
                 try {
-//                    // test 1
-//                    if (rand == 1) {
-//                        System.out.println("***** TEST 1 *****");
-//                        throw new IOException("test 1");
-//                    }
-
                     int insertedDocs = session.withTransaction(transaction, txnOptions);
-
-                    // test 2
-                    if (rand == 2) {
-                        System.out.println("***** TEST 2 *****");
-                        throw new IOException("test 2");
-                    }
 
                     LOGGER.info(
                             "Inserted {} documents into collection {}.",
                             insertedDocs,
                             collection.getNamespace());
                 } catch (MongoBulkWriteException e) {
-                    // ignore duplicate key errors in case txn was already committed but client was not aware of it
+                    // for insertions, ignore duplicate key errors in case txn was already committed
+                    // but client was not aware of it
+
+                    // NOTE: upserts are idempotent in mongo, so no exceptions need to be caught for
+                    // redundant upserts
                     for (WriteError err : e.getWriteErrors()) {
                         if (err.getCode() != DUPLICATE_KEY_ERROR_CODE) {
                             LOGGER.error("Failed to commit with Mongo transaction.", e);
                             failedBulk.add(bulk);
+                        } else {
+                            LOGGER.error(String.format("write error here: %s", err.getMessage()));
                         }
-                        // TODO: catch other write errors
                     }
                     LOGGER.warn("Ignoring duplicate records");
                 } catch (Exception e) {
                     // save to a new list that would be retried
-                    LOGGER.error("Failed to commit with Mongo transaction.", e);
+                    LOGGER.error(
+                            String.format(
+                                    "Failed to commit with Mongo transaction – requeueing %d records",
+                                    bulk.size()),
+                            e);
                     failedBulk.add(bulk);
                 }
+                // TODO: [DE-3303] if needed, catch duplicate delete errors
             }
         }
         return failedBulk;
